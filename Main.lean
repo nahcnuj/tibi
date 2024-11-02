6fb2aca3325d293a54232754a44cc7bb8cade16a
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-11-02 05:08:16 +0000

    initialize a Lean project

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Tibi
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ Tibi.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Tibi` library.
+-- Import modules here that should be built as part of the library.
+import Tibi.Basic
