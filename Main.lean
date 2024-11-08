8d973bf7926df15e936cb4c557cec97d21aa4b81
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-11-08 20:13:39 +0000

    support stdin

--- Main.lean
+++ Main.lean
@@ -1,13 +1,12 @@
 import Tibi
 
 def main : List String → IO UInt32
+| [] | "-" :: _ => do
+    Tibi.run (← IO.getStdin)
 | file :: _ => do
     let file := System.FilePath.mk file
     if ← file.pathExists then
-      IO.FS.withFile file IO.FS.Mode.read Tibi.run
+      IO.FS.withFile file IO.FS.Mode.read (Tibi.run ∘ IO.FS.Stream.ofHandle)
     else
       IO.eprintln s!"no such file: {file}"
       return 1
-| [] => do
-    IO.eprintln "require an input filename"
-    return 1
--- Tibi/Top.lean
+++ Tibi/Top.lean
@@ -1,19 +1,19 @@
-import Tibi.StringReader
 import Tibi.Tokenizer
 
 namespace Tibi
 
-private partial def repl (n : Nat) (handle : IO.FS.Handle) : IO UInt32 := do
-  let line ← handle.getLine
+private partial def repl (n : Nat) (stream : IO.FS.Stream) : IO UInt32 := do
+  if ← stream.isTty then IO.print s!"{n}:> "
+  let line ← stream.getLine
   if not line.isEmpty then
     match tokenize line with
     | .ok ts =>
         IO.println ts
-        repl n.succ handle
+        repl n.succ stream
     | .error e =>
         IO.eprintln s!"{e} at line {n}"
         return 1
   else
     return 0
 
-def run : IO.FS.Handle → IO UInt32 := repl 1
+def run : IO.FS.Stream → IO UInt32 := repl 1
