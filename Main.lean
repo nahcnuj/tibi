a62d2e66e03dfbe3c8db123d82a3e253184f6d38
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-11-30 18:13:21 +0000

    catch an error in main

--- Main.lean
+++ Main.lean
@@ -6,10 +6,14 @@ def main : List String → IO UInt32
 | file :: _ => do
     let file := System.FilePath.mk file
     if ← file.pathExists then
-      let bin ← IO.FS.withFile file IO.FS.Mode.read (Tibi.run ∘ IO.FS.Stream.ofHandle)
-      let dest ← IO.getStdout
-      dest.write bin
-      return 0
+      try
+        let bin ← IO.FS.withFile file IO.FS.Mode.read (Tibi.run ∘ IO.FS.Stream.ofHandle)
+        let dest ← IO.getStdout
+        dest.write bin
+        return 0
+      catch e =>
+        IO.eprintln e
+        return 1
     else
       IO.eprintln s!"no such file: {file}"
       return 1
